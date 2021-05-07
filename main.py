481e8ee1bd37ff9d9ad7b5394ed393b2dcd3008c
7d <>
2021-05-07 03:14:39 +0000

    changed colorama to blessed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from colorama import Fore, init
-from random import randint
-init()
+from blessed import Terminal
+from random import randint, choice
+term = Terminal()
 
 while True:
-    print(Fore.GREEN+str(randint(0,1)), end="")
+    if choice([True, False]):
+        print(term.green(str(randint(0,1))), end="")
+    else:
+        print(term.bright_green(str(randint(0,1))), end="")
